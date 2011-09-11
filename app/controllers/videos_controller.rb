f1bbe5b1837a276dcbb0b345ca523b66eee44a5f
John Fan <john.fan@friendstock.org>
2011-09-10 20:47:18 -0400

    comment out cartographer

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -28,29 +28,29 @@ class VideosController < ApplicationController
     wtc = [40.711626, -74.010714]
     @videos = Video.all
     # example
-    @map = Cartographer::Gmap.new('map',
-                                    :type => 'terrain',
-                                    :center => wtc,
-                                    :zoom => 14,
-                                    :controls => [:zoom]
-                                 )
+    # @map = Cartographer::Gmap.new('map',
+    #                                 :type => 'terrain',
+    #                                 :center => wtc,
+    #                                 :zoom => 14,
+    #                                 :controls => [:zoom]
+    #                              )
 #   @map.zoom = :bound
-    @icon = Cartographer::Gicon.new()
-    @map.icons <<  @icon
-    marker1 = Cartographer::Gmarker.new(:name=> "taj_mahal", :marker_type => "Building",
-               :position => [27.173006,78.042086],
-               :info_window_url => "/url_for_info_content", :icon => @icon)
-
-    marker2 = Cartographer::Gmarker.new(:name=> "wtc", :marker_type => "Building",
-               :position => wtc,
-               :info_window_url => "/url_for_info_content", :icon => @icon)
-    marker3 = Cartographer::Gmarker.new(:name=> "raj_bhawan", :marker_type => "Building",
-               :position => [28.614309,77.201353],
-               :info_window_url => "/url_for_info_content", :icon => @icon)
-
-    @map.markers << marker1
-    @map.markers << marker2
-    @map.markers << marker3
+    # @icon = Cartographer::Gicon.new()
+    # @map.icons <<  @icon
+    # marker1 = Cartographer::Gmarker.new(:name=> "taj_mahal", :marker_type => "Building",
+    #            :position => [27.173006,78.042086],
+    #            :info_window_url => "/url_for_info_content", :icon => @icon)
+    # 
+    # marker2 = Cartographer::Gmarker.new(:name=> "wtc", :marker_type => "Building",
+    #            :position => wtc,
+    #            :info_window_url => "/url_for_info_content", :icon => @icon)
+    # marker3 = Cartographer::Gmarker.new(:name=> "raj_bhawan", :marker_type => "Building",
+    #            :position => [28.614309,77.201353],
+    #            :info_window_url => "/url_for_info_content", :icon => @icon)
+    # 
+    # @map.markers << marker1
+    # @map.markers << marker2
+    # @map.markers << marker3
   end
   
   def show
